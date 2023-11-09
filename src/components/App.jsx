b53a62abd6a37f6c92bfd0e9f6fe6819a24824c7
Tcp93001 <joseguerreroroman@yahoo.com.mx>
2023-11-09 16:41:41 -0600

    cambios de clase 5

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,109 +1,167 @@
-import { Component } from 'react';
+import { useEffect, useState } from 'react';
 import Header from './Header';
 import Form from './Form';
 import TodoList from './TodoList'
 import '../styles/App.css';
 
-class App extends Component {
-  state = {
-    todos: [],
-    todosOriginales: []
-    // showButton: true
+const App = () => {
+
+  const [todos, setTodos] = useState([])
+  const [show, setShow] = useState(true)
+
+  // Iniciando, setear en un useEffect mount el valor de todos
+  // useEffect(() => {
+  //   setTodos([
+  //     { title: "Tarea 1000000000", done: true },
+  //     { title: "Tarea 2", done: false },
+  //     { title: "Tarea 3", done: true },
+  //     { title: "Tarea 4", done: false },
+  //     { title: "Tarea 5", done: true },
+  //     { title: "Tarea 6", done: false },
+  //     { title: "Tarea 7", done: true },
+  //     { title: "Tarea 8", done: false },
+  //     { title: "Tarea 9", done: true },
+  //     { title: "Tarea 10", done: false },
+  //   ])
+  // }, [])
+
+  // Usar este useEffect cuando llame al servidor
+  // No usamos https, solo la version insegura
+  const URL = 'http://localhost:3000/todos'
+  useEffect(() => {
+    const getData = async () => {
+      // si quiero agregar control de errores, agrego un bloque try/catch
+      try{
+        const response = await fetch(URL)
+        const data = await response.json()
+        setTodos(data)
+      } catch(err) {
+        console.log(err)
+      }
+      // const response = await fetch(URL)
+      // const data = await response.json()
+      // setTodos(data)
+    };
+
+    getData();
+  }, [])
+
+
+  const handleClickDelete = (event, title) => {
+    const todosList = [...todos]
+    const index = todosList.findIndex(elem => elem.title === title)
+    if (index > -1) todosList.splice(index, 1)
+    setTodos(todosList)
   }
 
-  // handleClick = (event) => {
-  //   event.preventDefault()
-  //   this.setState({
-  //     todos: [
-  //       { title: "Tarea 1", done: true },
-  //       { title: "Tarea 2", done: false },
-  //       { title: "Tarea 3", done: true },
-  //       { title: "Tarea 4", done: false },
-  //       { title: "Tarea 5", done: true },
-  //       { title: "Tarea 6", done: false },
-  //       { title: "Tarea 7", done: true },
-  //       { title: "Tarea 8", done: false },
-  //       { title: "Tarea 9", done: true },
-  //       { title: "Tarea 10", done: false },
-  //     ],
-  //     // showButton: false
-  //   })
-  // }
-
-  componentDidMount() {
-    this.setState({
-      todos: [
-        { title: "Tarea 1000000000", done: true },
-        { title: "Tarea 2", done: false },
-        { title: "Tarea 3", done: true },
-        { title: "Tarea 4", done: false },
-        { title: "Tarea 5", done: true },
-        { title: "Tarea 6", done: false },
-        { title: "Tarea 7", done: true },
-        { title: "Tarea 8", done: false },
-        { title: "Tarea 9", done: true },
-        { title: "Tarea 10", done: false },
-      ],
-      todosOriginales: [
-        { title: "Tarea 1000000000", done: true },
-        { title: "Tarea 2", done: false },
-        { title: "Tarea 3", done: true },
-        { title: "Tarea 4", done: false },
-        { title: "Tarea 5", done: true },
-        { title: "Tarea 6", done: false },
-        { title: "Tarea 7", done: true },
-        { title: "Tarea 8", done: false },
-        { title: "Tarea 9", done: true },
-        { title: "Tarea 10", done: false },
-      ],
+  const changeProperty = (config, property, value) => {
+    return fetch(config.url, {
+      method: config.method,
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ [property]: value })
     })
   }
 
-  handleClickDelete = (event, index) => {
-    const todos = [...this.state.todos]
-    todos.splice(index, 1)
-    this.setState({ todos })
-  }
-
-  handleClickToggleDone = (event, index) => {
-    const todos = [...this.state.todos]
-    todos[index].done = !todos[index].done
-    this.setState({ todos })
-  }
-
-  handleClickReset = (e) => {
-    this.setState({
-      todos: [...this.state.todosOriginales]
+  const goToBackend = (config, data) => {
+    return fetch(config.url, {
+      method: config.method,
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
     })
   }
 
+  const handleClickToggleDone = async (event, title) => {
+    const task = todos.find(elem => elem.title === title);
+    if (task === undefined) return;
 
-  render() {
-    return (
-      <div className="wrapper">
-        <div className="card-frame">
-          <Header counter={this.state.todos.length} />
-          <TodoList
-            tasks={this.state.todos}
-            toggleFn={this.handleClickToggleDone}
-            deleteFn={this.handleClickDelete}
-          />
-          <Form />
+    const value = !task.done;
+    const config = {
+      url: `${URL}/${task.id}`,
+      method: 'PATCH'
+    };
 
-          <button onClick={this.handleClickReset}>Reestablecer tareas</button>
+    try {
+      const response = await changeProperty(config, "done", value)
+
+      if (!response.ok) throw new Error("Response not ok");
+
+      // UI
+      const t = [...todos];
+      const index = t.findIndex(element => element.id === task.id);
+      t[index].done = !t[index].done;
+
+      setTodos(t);
+    } catch(err) {
+      console.error(err)
+    }
+
+    // const todosList = [...todos]
+    // const index = todosList.findIndex(elem => elem.title === title)
+    // if (index > -1) todosList[index].done = !todosList[index].done
+    // setTodos(todosList)
+  }
+
+  const handleAddTask = async (title) => {
+    const itExists = todos.find(elem => elem.title === title)
+    if (itExists) {
+      alert('Esta tarea ya existe')
+      return
+    }
+
+    
+    // // Cambio en el servidor
+    const config = {
+      url: URL,
+      method: "POST"
+    };
+
+    const data = {
+      title: title,
+      done: false,
+    };
+
+    try {
+      const response = await goToBackend(config, data);
+      if (!response.ok) throw new Error("Response not ok");
+
+      const todo = await response.json();
+
+      // UI
+      setTodos(todos.concat([todo]));
+    } catch (error) {
+      console.error(error);
+    }
+
+    // const todosList = [...todos]
+    // setTodos(todosList.concat([{ title, done: false }]))
 
 
-          {/* {
-            this.state.showButton ?
-              <button onClick={this.handleClick}>
-                Inicializar
-              </button>
-              : null
-          } */}
-        </div>
+  }
+
+  const filterTodos = todos.filter(elem => !elem.done || elem.done === show)
+
+  return (
+    <div className="wrapper">
+      <div className="card-frame">
+        <Header
+          counter={todos.length}
+          show={show}
+          toggleDone={setShow}
+        />
+        <TodoList
+          tasks={filterTodos}
+          toggleFn={handleClickToggleDone}
+          deleteFn={handleClickDelete}
+        />
+        <Form addTaskFn={handleAddTask} />
+
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 export default App;
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,12 +1,39 @@
+import { useState } from "react";
+import PropTypes from 'prop-types';
 import '../styles/Form.css'
 
-const Form = () => {
+const Form = ({ addTaskFn }) => {
+  const [value, setValue] = useState('')
+
+  const handleChange = (event) => {
+    setValue(event.target.value)
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    addTaskFn(value)
+    setValue('')
+  }
+
   return (
-    <form className="form-style">
-      <input className="add-task" placeholder="Capture la tarea a ejecutar" />
+    <form
+      onSubmit={handleSubmit}
+      className="form-style"
+    >
+      <input
+        placeholder="Capture la tarea a ejecutar"
+        type="text"
+        className="add-task"
+        value={value}
+        onChange={handleChange}
+      />
       <button>Agregar Tarea</button>
     </form>
   )
+}
+
+Form.propTypes ={
+  addTaskFn: PropTypes.func.isRequired
 }
 
 export default Form;
